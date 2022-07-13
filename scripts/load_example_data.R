057633db59348fb559f32076ff8476c51e9035b3
Aaron Gullickson <aarongullickson@gmail.com>
2022-07-13 13:25:27 -0700

    Add load_example_data.R script
    This script loads data directly from GitHub

---
+++ scripts/load_example_data.R
@@ -0,0 +1,3 @@
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/titanic.RData"))
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/movies.RData"))
+load(url("https://github.com/AaronGullickson/stat_data/raw/main/output/sex.RData"))

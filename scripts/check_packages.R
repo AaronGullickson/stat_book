6ab0070d43a7c38fed7a4e57f41c3257a3151bfb
Aaron Gullickson <aarongullickson@gmail.com>
2023-08-12 14:16:13 -0700

    Add measuring association chapter
    Still needs quite a bit of editing

--- scripts/check_packages.R
+++ scripts/check_packages.R
@@ -5,9 +5,10 @@
 #http://www.vikram-baliga.com/blog/2015/7/19/a-hassle-free-way-to-verify-that-r-packages-are-installed-and-loaded
 
 packages = c("here",
+             "MASS",
              "tidyverse", "janitor", "glue",
              "scales",
-             "gt", "gtExtras")
+             "gt", "gtExtras", "gganimate")
 
 #packages = c("here","tidyverse", "broom","janitor","glue",
 #             "ggalt","scales","ggrepel","gganimate","gifski",
